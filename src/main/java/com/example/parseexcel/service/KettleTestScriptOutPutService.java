16f3a40c4864f31f49145a9c5b9ac2f363f5a034
Henry <studyliuhuan@163.com>
2024-03-14 12:01:23 +0800

    值列表验证逻辑添加

--- src/main/java/com/example/parseexcel/service/KettleTestScriptOutPutService.java
+++ src/main/java/com/example/parseexcel/service/KettleTestScriptOutPutService.java
@@ -22,21 +22,57 @@ public class KettleTestScriptOutPutService {
                 "  temp_table\n"  + 
                 "UNION ALL";
 
-    public String autoMiddleSql(String databaseName, String middleTableName,String targetTableName, Map<String, String> data){
+    String templateStr3 = "\nUNION ALL\n" + 
+                "SELECT\n" + 
+                "CONCAT('%s','_', IFNULL(%s,'null')) AS 'column_name',\r\n" + 
+                "count AS 'count',\r\n" + 
+                "'%s' AS 'database_name',\n" + 
+                "'%s' AS 'table_name',\n" + 
+                "'值列表校验' AS 'check_type',\n" + 
+                "'原数据库' AS 'data_type'\n" + 
+                "FROM (\n" + 
+                "SELECT\n" + 
+                "%s AS %s,\n" + 
+                "count(*) AS 'count'\n" + 
+                "FROM %s GROUP BY %s\n" + 
+                ") AS m";
+    /**
+     * 
+     * @param databaseName 目标数据库名称
+     * @param middleTableName 中间表名称
+     * @param targetTableName 目标表名称
+     * @param countMap 数量校验
+     * @param valueMap 值列表校验
+     * @return
+     */
+    public String autoMiddleSql(String databaseName, String middleTableName,String targetTableName, Map<String, String> countMap, Map<String, String> valueMap){
         String dataStr = "";
         String dataStr2 = "";
-        for(Entry<String, String> entry : data.entrySet()){
+        for(Entry<String, String> entry : countMap.entrySet()){
             dataStr += String.format(templateStr, entry.getKey(), entry.getValue(),entry.getValue());
             dataStr2 += String.format(templateStr2, entry.getValue(),entry.getValue()
                         , databaseName, targetTableName);
         }
+        //处理最后的 UNION ALL
+        dataStr2 = dataStr2.substring(0, dataStr2.lastIndexOf("UNION ALL"));
+        
         String str1 = "WITH temp_table AS (\n" + 
             "SELECT\n" + 
-            dataStr.substring(dataStr.lastIndexOf(',', dataStr.length())) +
+            dataStr.substring(0, dataStr.lastIndexOf(',')) +
             "\nFROM " + middleTableName + " t" + 
             "\n)";
+        
+        //值列表校验
+        String dataStr3 = "";
+        for (Entry<String, String> entry : valueMap.entrySet()) {
+            dataStr3 += String.format(templateStr3, entry.getValue(), entry.getValue(), databaseName, targetTableName, entry.getKey(), entry.getValue(), middleTableName, entry.getKey());
+        }
 
-        return str1 + dataStr2;
+        return str1 + dataStr2 + dataStr3;
+    }
+
+    public String autoTargetSql(String databaseName, String targetTableName, Map<String, String> countMap, Map<String, String> valueMap){
+        return autoMiddleSql(databaseName, targetTableName, targetTableName, countMap, valueMap);
     }
 
     public static void main(String[] args) {
@@ -45,7 +81,10 @@ public class KettleTestScriptOutPutService {
         map.put("ssc_public_id", "SSC_PUBLIC_ID");
         map.put("SSCCODE", "SSC_CODE");
 
-        System.out.println(new KettleTestScriptOutPutService().autoMiddleSql("infra-dms-wty", "srv_ssc_m_middle", "t_wty_ssc", map)); 
+        Map<String, String> valueMap = new TreeMap<>();
+        valueMap.put("DELETEFLAG", "del_flag");
+
+        System.out.println(new KettleTestScriptOutPutService().autoMiddleSql("infra-dms-wty", "srv_ssc_m_middle", "t_wty_ssc", map, valueMap)); 
     }
     
 }
