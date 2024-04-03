3ad759429eb902fffbc360f16041b7dd55e31d5d
Henry <studyliuhuan@163.com>
2024-04-03 17:50:43 +0800

    kettle脚本controller添加

---
+++ src/main/java/com/example/parseexcel/controller/KettleOutPutController.java
@@ -0,0 +1,27 @@
+package com.example.parseexcel.controller;
+
+import com.example.parseexcel.service.KettleScriptOutPutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * @author liuhuan
+ * @date 2024/04/03
+ */
+@Controller
+@RequestMapping("simpleParse")
+public class KettleOutPutController {
+
+    @Autowired
+    KettleScriptOutPutService kettleScriptOutPutService;
+
+    public ResponseEntity<byte[]> outPutScript(){
+
+        return null;
+    }
+ 
+
+}
