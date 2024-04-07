2fd06be2c5cacf931eef2fd601b384bbb51f30ef
Henry <studyliuhuan@163.com>
2024-04-07 10:01:36 +0800

    查询

--- src/main/java/com/example/parseexcel/service/data/impl/TransferDbMappingServiceImpl.java
+++ src/main/java/com/example/parseexcel/service/data/impl/TransferDbMappingServiceImpl.java
@@ -1,11 +1,18 @@
 package com.example.parseexcel.service.data.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.parseexcel.dao.TransferDbMappingTableMapping;
 import com.example.parseexcel.dao.dto.TransferDbMappingTableDTO;
 import com.example.parseexcel.dao.model.TransferDbMappingTable;
 import com.example.parseexcel.service.data.TransferDbMappingService;
+
+import org.apache.commons.beanutils.PropertyUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author liuhuan
@@ -14,9 +21,19 @@ import java.util.List;
 @Service
 public class TransferDbMappingServiceImpl implements TransferDbMappingService {
 
+    @Autowired
+    TransferDbMappingTableMapping transferDbMappingTableMapping;
 
     public List<TransferDbMappingTable> list(TransferDbMappingTableDTO transferDbMappingTableDTO){
-
-        return  null;
+        Map<String, Object> queryMap = new HashMap<>();
+        try {
+            queryMap = PropertyUtils.describe(transferDbMappingTableDTO);
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        QueryWrapper<TransferDbMappingTable> queryWrapper = new QueryWrapper<>();
+        queryWrapper.allEq(queryMap);
+        return  transferDbMappingTableMapping.selectList(queryWrapper);
     }
 }
