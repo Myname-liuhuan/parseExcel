a5c447e7c4fe8e990e3d0d6d497b281a072a8d77
liuhuan <1171814970@qq.com>
2024-04-16 23:40:41 +0800

    xml验证

---
+++ src/main/java/com/example/parseexcel/common/utils/XMLUtils.java
@@ -0,0 +1,21 @@
+package com.example.parseexcel.common.utils;
+
+
+import java.util.Queue;
+
+/**
+ * @author liuhuan
+ * @date 2024/4/16
+ */
+public class XMLUtils {
+
+    /**
+     * 验证文本是否是xml格式
+     * @param text
+     * @return
+     */
+    public static boolean checkXml(String text){
+
+        return false;
+    }
+}
