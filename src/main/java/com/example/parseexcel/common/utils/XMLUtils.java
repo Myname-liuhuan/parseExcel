5583e80ab9b29e6608cacd0c0414bbd619c41ef2
liuhuan <1171814970@qq.com>
2024-04-17 22:33:09 +0800

    xml验证逻辑

--- src/main/java/com/example/parseexcel/common/utils/XMLUtils.java
+++ src/main/java/com/example/parseexcel/common/utils/XMLUtils.java
@@ -2,6 +2,7 @@ package com.example.parseexcel.common.utils;
 
 
 import java.util.Queue;
+import java.util.Stack;
 
 /**
  * @author liuhuan
@@ -9,13 +10,56 @@ import java.util.Queue;
  */
 public class XMLUtils {
 
+    private String text;
+
+    private XMLUtils(String text){
+        this.text = text;
+    }
+
+
+    public  boolean checkXml(){
+        Stack<String> stack = new Stack<>();
+        for(int i = 0; i < text.length(); i++){
+            char c = text.charAt(i);
+            if ('<' == c){
+                String left = getLeft(i, 1);
+            }
+        }
+        return false;
+    }
+
     /**
-     * 验证文本是否是xml格式
-     * @param text
+     * 获取左标签
+     * @param startIndex 开始索引
+     * @param len 字符长度
      * @return
      */
-    public static boolean checkXml(String text){
+    private String getLeft(Integer startIndex, Integer len){
+        char c = text.charAt(startIndex + len);
+        if (Character.isSpaceChar(c)){
+            return text.substring(startIndex, startIndex + len);
+        }else {
+            return getLeft(startIndex, len + 1);
+        }
+    }
 
-        return false;
+    /**
+     * 获取右标签
+     * @param endIndex
+     * @param len
+     * @return
+     */
+    private String getRight(Integer endIndex, Integer len){
+        char c = text.charAt(endIndex - len);
+        if (Character.isSpaceChar(c)){
+            return text.substring(endIndex - len, endIndex);
+        }else {
+            return getRight(endIndex, len + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        XMLUtils xmlUtils = new XMLUtils("<1234 dfaf");
+        System.out.println(xmlUtils.getLeft(0, 1));
     }
 }
