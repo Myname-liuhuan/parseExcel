7f866c8a97f9ebda8c1314eae18e6da99afaf9f7
Henry <studyliuhuan@163.com>
2024-06-07 17:25:44 +0800

    mybatis原生分页配置

---
+++ src/main/java/com/example/parseexcel/common/config/MybatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.example.parseexcel.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+
+
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 注入拦截器，实现mybatis原生分页效果
+     * 分页插件
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
