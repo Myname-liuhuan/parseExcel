44fdd32a63b2ae94d6597a2741cf44e7a7308cd5
liuhuan <1171814970@qq.com>
2024-06-08 22:27:31 +0800

    添加返回值封装类

---
+++ src/main/java/com/example/parseexcel/common/result/IErrorCode.java
@@ -0,0 +1,14 @@
+package com.example.parseexcel.common.result;
+
+
+public interface IErrorCode {
+    /**
+     * 返回码
+     */
+    long getCode();
+
+    /**
+     * 返回信息
+     */
+    String getMessage();
+}
