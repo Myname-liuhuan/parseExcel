861790fa4c1449bea6d85208f23f4d81abfb742c
Henry <studyliuhuan@163.com>
2024-07-10 10:23:27 +0800

    逻辑优化

--- src/main/java/com/example/parseexcel/common/utils/ImageUtil.java
+++ src/main/java/com/example/parseexcel/common/utils/ImageUtil.java
@@ -6,20 +6,24 @@ import java.io.IOException;
 import net.coobird.thumbnailator.Thumbnails;
 
 public class ImageUtil {
+
+    private static String FIRST_SOURCE_DIRECTORY;
+    private static String FIRST_TARGET_DIRECTORY;
+
     
     /**
      * 生成缩略图
      * @param sourceImage 指向文件/目录的路径
      * @param targetDirectory 指向文件夹的路径
      */
-    public static void generateThumbnails(File sourceImage, String targetDirectory){
+    private static void generateThumbnails(File sourceImage){
         if (sourceImage.isFile()) {
             try {
                 Thumbnails.of(sourceImage)
                     //设置缩略图大小，按等比缩放
                     .size(200, 200)
                     //将生成的缩略图写入文件
-                    .toFile(new File(targetDirectory + "/" + sourceImage.getName()));
+                    .toFile(new File(sourceImage.getAbsolutePath().replace(FIRST_SOURCE_DIRECTORY, FIRST_TARGET_DIRECTORY)));
             } catch (IOException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
@@ -27,9 +31,20 @@ public class ImageUtil {
         }else{
             File[] files = sourceImage.listFiles();
             for (File file : files) {
-                generateThumbnails(file, targetDirectory);
+                generateThumbnails(file);
             }
         }
+    }
+
+    /**
+     * 生成缩略图,且包含子文件夹
+     * @param sourceDirectoy
+     * @param targetDirectory
+     */
+    public static void generateThumbnails(String sourceDirectoy, String targetDirectory){
+        FIRST_SOURCE_DIRECTORY = new File(sourceDirectoy).getAbsolutePath();
+        FIRST_TARGET_DIRECTORY = new File(targetDirectory).getAbsolutePath();
+        generateThumbnails(new File(FIRST_SOURCE_DIRECTORY));
     }
 
 }
