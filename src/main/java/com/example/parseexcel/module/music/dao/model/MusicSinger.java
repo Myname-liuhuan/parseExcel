d98d6a530b4ba84453b496d1ff47483ea882c111
Henry <studyliuhuan@163.com>
2024-08-26 17:28:18 +0800

    singer公共字段也添加默认值

--- src/main/java/com/example/parseexcel/module/music/dao/model/MusicSinger.java
+++ src/main/java/com/example/parseexcel/module/music/dao/model/MusicSinger.java
@@ -2,7 +2,9 @@ package com.example.parseexcel.module.music.dao.model;
 
 import java.util.Date;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -33,15 +35,20 @@ public class MusicSinger {
     private Date birthday;
 
 /*** 以下为公共字段 ***/
+    @TableField(fill = FieldFill.INSERT)
     private Long createUser;
 
+    @TableField(fill = FieldFill.UPDATE)
     private Long updateUser;
 
+    @TableField(fill = FieldFill.INSERT)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createTime;
 
+    @TableField(fill = FieldFill.UPDATE)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
 
+    @TableField(fill = FieldFill.INSERT)
     private Integer delFlag;
 }
