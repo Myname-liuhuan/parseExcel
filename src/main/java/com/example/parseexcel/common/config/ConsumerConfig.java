74df1c9f44de14b94509d4af3f9e254ff9d6ffb1
liuhuan <1171814970@qq.com>
2024-10-13 12:24:30 +0800

    添加mq配置及静态推送Config类

---
+++ src/main/java/com/example/parseexcel/common/config/ConsumerConfig.java
@@ -0,0 +1,24 @@
+package com.example.parseexcel.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Consumer;
+
+/**
+ * @author liuhuan
+ * @date 2024/10/13
+ */
+@Configuration
+public class ConsumerConfig {
+
+    /**
+     * 消费端，@bean注解的方法名需要和配置文件里面的一样
+     * @return
+     */
+    @Bean
+    public Consumer<String> musicInfoConsumer(){
+        return msgData -> System.out.println(msgData);
+    }
+
+}
---
+++ src/main/java/com/example/parseexcel/common/config/ProducerConfig.java
@@ -0,0 +1,30 @@
+package com.example.parseexcel.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+
+import java.util.function.Supplier;
+
+/**
+ * @author liuhuan
+ * @date 2024/10/13
+ */
+@Configuration
+public class ProducerConfig {
+
+    private int id = 1;
+
+    /**
+     * 发送 静态消息，
+     * @bean注解的方法名需要和配置文件里面的一样
+     * 没有手动指定的情况下，每秒发送一条消息
+     * @return
+     */
+    @Bean
+    public Supplier<Message<String>> musicInfoProducer() {
+        return () -> MessageBuilder.withPayload(String.format("第%d条消息", id++))
+                .build();
+    }
+}
